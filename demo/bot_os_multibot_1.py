2bdb85b67b4e435938db7a196b3629b9f57f9805
genesis-gh-jlangseth <justin.langseth@genesiscomputing.ai>
2024-06-23 14:55:08 +0200

    version 141

--- demo/bot_os_multibot_1.py
+++ demo/bot_os_multibot_1.py
@@ -67,7 +67,7 @@ logging.basicConfig(
 import core.global_flags as global_flags
 
 
-print("****** GENBOT VERSION 0.140 *******")
+print("****** GENBOT VERSION 0.141 *******")
 
 runner_id = os.getenv("RUNNER_ID", "jl-local-runner")
 global_flags.runner_id = runner_id

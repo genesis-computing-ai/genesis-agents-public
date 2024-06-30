f39ab99943c1b7e3450c77ef9266cdb430093228
mg23 <mg23.helloworld@gmail.com>
2024-06-30 17:12:42 -0400

    datacube works

--- streamlit_gui/streamlit_show_datacube.py
+++ streamlit_gui/streamlit_show_datacube.py
@@ -22,21 +22,22 @@ def get_expand_paths_filter(df:pd.DataFrame, column_names:list[str], open_paths:
                 filter_strings_with_locations.append((filter_string, location[0]))
     return filter_strings_with_locations
 
-def expand_paths(df:pd.DataFrame, open_paths:list[pd.DataFrame], base_query:str, column_names:list, column_types:dict, group_by_columns:list[str], filter_conditions:str,
+def expand_paths(df:pd.DataFrame, open_paths:list[pd.DataFrame], base_query:str, column_names:list, column_types:dict, group_by_columns:list[str], filter_conditions:list[str],
                  expand_path_level:int) -> pd.DataFrame:
-    if expand_path_level > len(group_by_columns):
+    if expand_path_level > len(group_by_columns)+1:
         return df
     filters_with_locations = get_expand_paths_filter(df, column_names, open_paths, group_by_columns[:expand_path_level])
     expanded_dfs = []
     inner_group_by_columns = group_by_columns[expand_path_level:]
+    outter_group_by_columns= group_by_columns[:expand_path_level]
     for filter, location in filters_with_locations:
         if filter_conditions is None:
-            filter_conditions_per = filter
+            filter_conditions_per = [filter]
         else:
-            filter_conditions_per = f"({filter_conditions}) AND ({filter})"
-        df2, df2_col_names, df2_col_types = use_run_query(base_query, column_names, column_types, group_by_columns=inner_group_by_columns, filter_conditions=[filter_conditions_per],
-                            expand_open_paths=True, expand_path_level=expand_path_level + 1)
-        df2[group_by_columns] = '+'
+            filter_conditions_per = filter_conditions + [filter]
+        df2, df2_col_names, df2_col_types = use_run_query(base_query, column_names, column_types, group_by_columns=inner_group_by_columns, filter_conditions=filter_conditions_per,
+                            expand_open_paths=True, expand_path_level=expand_path_level)
+        df2[outter_group_by_columns] = '+'
         expanded_dfs.append((filter, location, df2))
 
     output_df = df.copy()
@@ -45,6 +46,7 @@ def expand_paths(df:pd.DataFrame, open_paths:list[pd.DataFrame], base_query:str,
         offset = 0
         for filter, location, df2 in expanded_dfs:
             insert_location = location + offset + 1
+            output_df.at[insert_location-1, 'is_expanded'] = True
             output_df = pd.concat([output_df.iloc[:insert_location], df2, output_df.iloc[insert_location:]]).reset_index(drop=True)
             offset += len(df2)
     return output_df
@@ -54,7 +56,8 @@ def use_run_query(query, column_names:list=["*"], column_types:dict={}, group_by
                   expand_open_paths=False, expand_path_level=0):
     open_paths = st.session_state.get('open_paths', [])
     base_column_names = column_names
-    
+    base_group_by_columns = group_by_columns
+
     print(f"use_run_query - open_paths = {open_paths}")
     
     # Initialize SnowflakeConnector
@@ -66,22 +69,37 @@ def use_run_query(query, column_names:list=["*"], column_types:dict={}, group_by
     # Construct the CTE
     cte_query = f"WITH subquery AS ({base_query})"
     
+    # Truncate the group_by_columns list to only include columns up to the current expand_path_level
+    group_by_columns = group_by_columns[:expand_path_level+1]
+
     # Remove group_by_columns from column_names
+    column_names = [col for col in column_names if col != '__UNIQUE_ID__']
     if group_by_columns and len(group_by_columns) > 0:
+        column_names_with_aliases = [
+            f"sum({col}) as {col}" if column_types.get(col,'') in ['int', 'float'] else f"min({col}) as {col}"
+            for col in column_names if col not in group_by_columns
+        ]
         column_names = [
             f"sum({col})" if column_types.get(col,'') in ['int', 'float'] else f"min({col})"
             for col in column_names if col not in group_by_columns
         ]
+    else:
+        column_names_with_aliases = column_names
+
     # Start the main query
-    group_by_str = ','.join(group_by_columns) if group_by_columns else ''
+    group_by_str = ', '.join(group_by_columns) if group_by_columns else ''
     if len(group_by_columns) == 1:
         group_by_str = group_by_columns[0]
-    column_names_str = ','.join(column_names) if len(column_names) > 1 else ''.join(column_names)
+    column_names_str = ', '.join(column_names) if len(column_names) > 1 else ''.join(column_names)
+    if '*' in column_names:
+        column_names_str_with_aliases = column_names_str
+    else: 
+        column_names_str_with_aliases = ', '.join(column_names_with_aliases)
+    unique_id_expr = f"HASH(TO_VARIANT(CONCAT({column_names_str}))) as __UNIQUE_ID__"
     if group_by_str:
-        main_query = f"SELECT {group_by_str}, {column_names_str} FROM subquery"
+        main_query = f"SELECT {group_by_str}, {column_names_str_with_aliases}, {unique_id_expr} FROM subquery"
     else:
-        main_query = f"SELECT {column_names_str} FROM subquery"
-    
+        main_query = f"SELECT {column_names_str_with_aliases}, {unique_id_expr} FROM subquery"
     if filter_conditions:
         main_query += " WHERE " + " AND ".join(filter_conditions)
     
@@ -102,11 +120,12 @@ def use_run_query(query, column_names:list=["*"], column_types:dict={}, group_by
         column_types = {}
     else:
         if result_set:
-            column_names = list(result_set[0].keys())
+            column_names = [col for col in result_set[0].keys()]# if col != '__UNIQUE_ID__']
             df = pd.DataFrame(result_set, columns=column_names)
+            df.insert(0, 'is_expanded', False)
             column_types = {col: type(result_set[0][col]).__name__ for col in column_names}
             if open_paths and expand_open_paths:
-                df = expand_paths(df, open_paths, base_query, base_column_names, column_types, group_by_columns, 
+                df = expand_paths(df, open_paths, base_query, base_column_names, column_types, base_group_by_columns, 
                                   filter_conditions, expand_path_level+1)
         else:
             df = pd.DataFrame()  # Return an empty DataFrame if result_set is empty
@@ -143,33 +162,32 @@ def main():
                                                 group_by_columns=selected_group_by, filter_conditions=filter_conditions,
                                                 expand_open_paths=True)
                                                 #open_paths = st.session_state.get('open_paths', []))
-        
+        #result_df.insert(0, 'is_expanded', False)
+    
         st.write("Select rows to expand/collapse:")
         
-        # Display the dataframe with a single expand button
-        event = st.dataframe(result_df, use_container_width=True, on_select="rerun", selection_mode="single-row") #, 
-        if event.selection.rows != st.session_state.get('selected_rows', []) and len(result_df) == st.session_state.get('result_df_len',0):
-            st.session_state.open_paths = [result_df.iloc[r] for r in event.selection.rows]
-            st.session_state.selected_rows = event.selection.rows
-            # selected_row_indices = event.selection.rows
-            # open_paths = st.session_state.get('open_paths', [])
-            
-            # for row_index in selected_row_indices:
-            #     row_data = result_df.iloc[row_index]
-            #     if row_data in open_paths:
-            #         open_paths.remove(row_data)
-            #     else:
-            #         open_paths.append(row_data)
-            
-            # st.session_state.open_paths = open_paths
-            # st.session_state.selected_rows = selected_row_indices
-            st.rerun()
-
-        #event.selection.rows = st.session_state.get('selected_rows', [])
-
-        st.session_state.result_df_len = len(result_df)
+        def reset_cube():
+            st.session_state.open_paths = []
+        st.button("Reset", on_click=reset_cube)
         
+        def data_change():
+            print(st.session_state["my_data_editor_key"])
+            expanded_rows = st.session_state.get('open_paths', [])
+
+            edited_rows = st.session_state["my_data_editor_key"].get('edited_rows', {})
+            for row_index, changes in edited_rows.items():
+                if 'is_expanded' in changes:
+                    row = result_df.iloc[int(row_index)]
+                    if changes['is_expanded']: 
+                        if row['__UNIQUE_ID__'] not in [r['__UNIQUE_ID__'] for r in expanded_rows]:
+                            expanded_rows.append(row)
+                    elif not changes['is_expanded']:
+                        expanded_rows = [row for row in expanded_rows if row['__UNIQUE_ID__'] != result_df.iloc[int(row_index)]['__UNIQUE_ID__']]
+            st.session_state.open_paths = expanded_rows
+
+        editable = st.data_editor(result_df, use_container_width=True, key="my_data_editor_key", on_change=data_change)
         st.write(f"{len(result_df)} rows returned.")
+
     else:
         st.write("Please provide a SQL query in the URL parameter `sql_query`.")
 if __name__ == "__main__":

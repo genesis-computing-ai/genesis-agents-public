1f38979f9c938c06525b3e422bf0817a45945956
Michael Rainey <michael.rainey@genesiscomputing.ai>
2024-07-16 09:07:27 -0700

    update gemini api key with default value

--- demo/gemini_demo_1.py
+++ demo/gemini_demo_1.py
@@ -1,7 +1,8 @@
 import google.generativeai as genai
 import os
 
-genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+# genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+genai.configure(api_key=os.getenv('GEMINI_API_KEY','AIzaSyAI1muEgo3HtNCRiPbt7tNsP-iZLoZpISI'))
 
 for m in genai.list_models():
   if 'generateContent' in m.supported_generation_methods:

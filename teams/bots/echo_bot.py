90d326a0e41c6021a8e258a735effcde1c5350c3
genesis-gh-jlangseth <justin.langseth@genesiscomputing.ai>
2024-07-31 10:15:18 -1000

    teams - made echobot more fun, adding stuff to read.me

--- teams/bots/echo_bot.py
+++ teams/bots/echo_bot.py
@@ -4,6 +4,7 @@
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
 
+import time
 
 class EchoBot(ActivityHandler):
     async def on_members_added_activity(
@@ -13,9 +14,14 @@ class EchoBot(ActivityHandler):
             if member.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity("Hello and welcome!")
                 MessageFactory.text(f"member id: {member.id}" )
-                MessageFactory.text(f"You said: {turn_context.activity.recipient.id}")
+                MessageFactory.text(f"You said ffff: {turn_context.activity.recipient.id}")
 
     async def on_message_activity(self, turn_context: TurnContext):
+        print('got message: ',turn_context.activity.text)
+        print('waiting 1')
+        time.sleep(1)
+        print('waited...')
+        resp = 'Genbot says: '+turn_context.activity.turn_context.reverse()+' haha!'
         return await turn_context.send_activity(
-            MessageFactory.text(f"You said: {turn_context.activity.text}")
+            MessageFactory.text(f"{resp}")
         )

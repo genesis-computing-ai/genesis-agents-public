f9055061accf38a506bb02e0a08a88a9f760076c
Reza Vaghefi <reza.vaghefi@genesiscomputing.ai>
2024-08-15 11:29:59 -0700

    fixed knowledge server to work with llmhandler

--- knowledge/bot_os_knowledge.py
+++ knowledge/bot_os_knowledge.py
@@ -8,13 +8,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 
 from connectors.bigquery_connector import BigQueryConnector
 from connectors.snowflake_connector import SnowflakeConnector
+from connectors.sqlite_connector import SqliteConnector
 from knowledge.knowledge_server import KnowledgeServer
 from core.bot_os_llm import LLMKeyHandler
 import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s')
 
-genesis_source = os.getenv('GENESIS_SOURCE',default="BigQuery")
+genesis_source = os.getenv('GENESIS_SOURCE', default="Snowflake")
 
 #print("waiting 60 seconds for other services to start first...", flush=True)
 #time.sleep(1)
@@ -27,11 +28,14 @@ logger.info('Starting DB connection...')
 if genesis_source == 'BigQuery':
     credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS',default=".secrets/gcp.json")
     with open(credentials_path) as f:
-        connection_info = json.load(f)
-    # Initialize BigQuery client
-    knowledge_db_connector = BigQueryConnector(connection_info,'BigQuery')
-else:    # Initialize BigQuery client
+        connection_info = json.load(f)    
+    knowledge_db_connector = BigQueryConnector(connection_info,'BigQuery') # Initialize BigQuery client
+elif genesis_source ==  'Sqlite':
+    knowledge_db_connector = SqliteConnector(connection_name='Sqlite')
+elif genesis_source == 'Snowflake':   
     knowledge_db_connector = SnowflakeConnector(connection_name='Snowflake')
+else:
+    raise ValueError('Invalid Source')
 
 
 logger.info('Getting LLM API Key...')
@@ -56,7 +60,7 @@ def get_llm_api_key(db_adapter=None):
         llm_key_handler = LLMKeyHandler(db_adapter)
         logger.info('Getting LLM API Key...')
 
-        api_key_from_env, llm_api_key = llm_key_handler.get_llm_key_from_db()
+        api_key_from_env, llm_api_key, llm_type = llm_key_handler.get_llm_key_from_db()
 
         if llm_api_key is None and llm_api_key != 'cortex_no_key_needed':
         #   print('No LLM Key Available in ENV var or Snowflake database, sleeping 20 seconds before retry.', flush=True)

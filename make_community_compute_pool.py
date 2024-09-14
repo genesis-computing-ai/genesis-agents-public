d7b1e01fbbf5dc4ee786151e36bd19af09f69903
Michael Rainey <michael.rainey@genesiscomputing.ai>
2024-09-13 19:17:37 -0700

    replace genesis_eai with genesis_community_eai

--- make_community_compute_pool.py
+++ make_community_compute_pool.py
@@ -11,6 +11,16 @@ def replace_genesis_bots(input_file, output_file):
     with open(output_file, 'w') as file:
         file.write(modified_content)
 
+def replace_genesis_bots_eai(input_file, output_file):
+    with open(input_file, 'r') as file:
+        content = file.read()
+    
+    # Replace the specific line
+    modified_content = re.sub(r'GENESIS_EAI', 'GENESIS_COMMUNITY_EAI', content)
+    
+    with open(output_file, 'w') as file:
+        file.write(modified_content)       
+
 # Get the directory of this script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -23,3 +33,13 @@ for file in files_to_change:
     # Usage
     replace_genesis_bots(input_file, output_file)
     print(f"Replacement complete. New file '{output_file}' created with GENESIS_COMMUNITY_POOL.")
+
+# Define input and output file paths relative to the script directory
+files_to_change = ['connectors/snowflake_connector.py','snowflake_app/setup_script.sql','streamlit_gui/main.py','streamlit_gui/sis_launch.py','streamlit_gui/page_files/config_eai.py','streamlit_gui/page_files/start_service.py','streamlit_gui/page_files/start_stop.py']
+for file in files_to_change:
+    input_file = os.path.join(script_dir, file)
+    output_file = os.path.join(script_dir, file)
+
+    # Usage
+    replace_genesis_bots_eai(input_file, output_file)
+    print(f"Replacement complete. New file '{output_file}' created with GENESIS_COMMUNITY_EAI.")

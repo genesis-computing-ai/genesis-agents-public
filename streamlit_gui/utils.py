28420751360c292a28e9eb6b447978dee81de581
genesis-gh-jlangseth <justin.langseth@genesiscomputing.ai>
2024-10-10 15:57:27 -1000

    fixed slack config for local run

--- streamlit_gui/utils.py
+++ streamlit_gui/utils.py
@@ -14,6 +14,7 @@ def get_session():
             return get_active_session()
         except:
             st.session_state.NativeMode = False
+            st.session_state.eai_available = True
   #  st.write('NativeMode', NativeMode)
     return None
 

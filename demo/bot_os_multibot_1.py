058447e35c289a83b0122f82748b69af0ae0a2f7
genesis-gh-jdavidson <jeff.davidson@genesiscomputing.ai>
2024-10-10 23:18:20 -0700

    Add print statement before one time fixes and ensure table exists

--- demo/bot_os_multibot_1.py
+++ demo/bot_os_multibot_1.py
@@ -155,7 +155,7 @@ if os.getenv("TEST_MODE", "false").lower() == "true":
     print("TEST_MODE - ensure table exists skipped")
     print("()()()()()()()()()()()()()")
 else:    
-   # db_adapter.remove_deprecated_tables()
+    print("NOT RUNNING TEST MODE - APPLYING ONE TIME DB FIXES AND CREATING TABLES")
     db_adapter.one_time_db_fixes()
     db_adapter.ensure_table_exists()
 

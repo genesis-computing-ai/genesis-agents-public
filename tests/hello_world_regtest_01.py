decb4416179b17a3a2f738799108b98cecde6c2a
Michael Rainey <michael.rainey@genesiscomputing.ai>
2024-10-29 09:45:57 -0700

    update test script to optionally use key pair auth

--- tests/hello_world_regtest_01.py
+++ tests/hello_world_regtest_01.py
@@ -2,16 +2,47 @@
 from snowflake.connector import SnowflakeConnection
 import os
 import time
-import uuid
+from cryptography.hazmat.primitives import serialization
+# import uuid
 # Build a SnowflakeConnection from env variables
-conn = SnowflakeConnection(
+
+
+
+# Load the private key from environment variable
+private_key = os.getenv('SNOWFLAKE_PRIVATE_KEY')
+
+if private_key:
+    # If your key is encrypted with a passphrase
+    passphrase = os.getenv('PRIVATE_KEY_PASSPHRASE')
+
+    # Convert the key to bytes
+    private_key_bytes = private_key.encode('utf-8')
+
+    # Load the private key using cryptography
+    p_key = serialization.load_pem_private_key(
+        private_key_bytes,
+        password=passphrase.encode('utf-8') if passphrase else None,
+    )
+
+    # Snowflake connection using private key authentication
+    conn = SnowflakeConnection(
+        user=os.getenv('SNOWFLAKE_USER_OVERRIDE'),
+        account=os.getenv('SNOWFLAKE_ACCOUNT_OVERRIDE'),
+        warehouse=os.getenv('SNOWFLAKE_WAREHOUSE_OVERRIDE'),
+        database=os.getenv('SNOWFLAKE_DATABASE_OVERRIDE'),
+        role=os.getenv("SNOWFLAKE_ROLE_OVERRIDE"),
+        private_key=p_key
+    )
+else:
+    conn = SnowflakeConnection(
     account=os.getenv("SNOWFLAKE_ACCOUNT_OVERRIDE"),
     user=os.getenv("SNOWFLAKE_USER_OVERRIDE"),
     password=os.getenv("SNOWFLAKE_PASSWORD_OVERRIDE"),
     database=os.getenv("SNOWFLAKE_DATABASE_OVERRIDE"),
     warehouse=os.getenv("SNOWFLAKE_WAREHOUSE_OVERRIDE"),
     role=os.getenv("SNOWFLAKE_ROLE_OVERRIDE")
-)
+    )
+
 
 # Execute the SQL code
 #grant_usage_1 = conn.cursor().execute("call genesis_bots.core.run_arbitrary('grant usage on function genesis_bots.app1.submit_udf(varchar, varchar, varchar) to application role app_public')")

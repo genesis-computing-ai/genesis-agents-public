5e41e4d3e0ffb25080e7fe1132543e6d521a0aaf
genesis-gh-jlangseth <justin.langseth@genesiscomputing.ai>
2024-11-07 08:17:59 -1000

    fix logging messages to include correct file, function, and line number that generated the log line

--- core/logging_config.py
+++ core/logging_config.py
@@ -8,17 +8,13 @@ def setup_logger(name=None):
     level = os.environ.get('LOG_LEVEL', 'INFO')
     logger.setLevel(level)
 
-    # Check if handlers are already added to avoid duplicate logs
     if not logger.handlers:
-        # Create handlers
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setLevel(level)
 
-        # Create formatters and add them to handlers
-        formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(module)s][%(lineno)d]:: %(message)s')
+        # Update formatter to use custom attributes
+        formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(caller_filename)s][%(caller_funcName)s:%(caller_lineno)s]:: %(message)s')
         console_handler.setFormatter(formatter)
-
-        # Add handlers to the logger
         logger.addHandler(console_handler)
 
     return logger
@@ -31,28 +27,35 @@ class PrintLikeLogger:
     def _format_message(self, *args):
         return ' '.join(str(arg) for arg in args)
     
+    def _get_caller_info(self):
+        return {
+            'caller_funcName': sys._getframe(2).f_code.co_name,
+            'caller_filename': os.path.basename(sys._getframe(2).f_code.co_filename),
+            'caller_lineno': sys._getframe(2).f_lineno
+        }
+    
     def info(self, *args, **kwargs):
         message = self._format_message(*args)
-        self._logger.info(message)
+        self._logger.info(message, extra=self._get_caller_info())
     
     def error(self, *args, **kwargs):
         message = self._format_message(*args)
-        self._logger.error(message)
+        self._logger.error(message, extra=self._get_caller_info())
     
     def warning(self, *args, **kwargs):
         message = self._format_message(*args)
-        self._logger.warning(message)
+        self._logger.warning(message, extra=self._get_caller_info())
         
     def debug(self, *args, **kwargs):
         message = self._format_message(*args)
-        self._logger.debug(message)
+        self._logger.debug(message, extra=self._get_caller_info())
         
     def critical(self, *args, **kwargs):
         message = self._format_message(*args)
-        self._logger.critical(message)
+        self._logger.critical(message, extra=self._get_caller_info())
         
     def exception(self, *args, **kwargs):
         message = self._format_message(*args)
-        self._logger.exception(message)
+        self._logger.exception(message, extra=self._get_caller_info())
 
 logger = PrintLikeLogger(setup_logger('GENSIS'))

bbce882584e5ebaa65a1ed64ff3dd01fb93e7843
genesis-gh-adekel <aviv.dekel@genesiscomputing.ai>
2024-11-19 12:14:00 -0500

    Add facility to supress certain logs messages (only show Nth message).  Apply to cnowflake connector messages polluting the native app.

--- core/logging_config.py
+++ core/logging_config.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+import re
 
 
 # Logging format use for root logger and GENESIS logger.
@@ -96,5 +97,78 @@ class PrintLikeLogger:
         message = self._format_message(*args)
         self._logger.telemetry(message, extra=self._get_caller_info())
 
+
+class LogSupressor:
+    """
+    A class to suppress repetitive log messages based on specified criteria.
+
+    This class allows you to define suppression rules for log messages
+    from specific modules. You can specify the log level, a regular
+    expression to match the log message, and the frequency of messages
+    to display. Messages that match the criteria will be suppressed
+    *after* the first occurrence, and only every nth message will be shown
+    thereafter.
+
+    Attributes:
+        _modulename_to_filtersspec_map (dict): A mapping of module names
+            to their respective filter specifications.
+    """
+    _modulename_to_filtersspec_map = dict()
+
+    class _FilterSpec:
+        def __init__(self, logger_name, log_level, regexp, n, counter=0):
+            if isinstance(log_level, str):
+                log_level = logging.getLevelName(log_level.upper())
+            self.logger_name = logger_name
+            self.log_level = log_level
+            self.regexp = regexp
+            self.n = n
+            self.counter = counter
+
+    @staticmethod
+    def _filter_record(record):
+        logger_name = record.name
+        specs = LogSupressor._modulename_to_filtersspec_map.get(logger_name, [])
+        show_record = True
+        for spec in specs:
+            # if the record matches the spec then increase the counter and determine if it should be filtered.
+            if record.levelno == spec.log_level and re.search(spec.regexp, record.getMessage()):
+                spec.counter += 1
+                show_record = spec.counter == 1 or spec.counter % spec.n == 1
+                if show_record and spec.counter > 1:
+                    record.msg += f" [NOTE: PREVIOUS {spec.n-1} SIMILAR RECORDS HAS BEEN SUPRESSED ({spec.regexp=})]"
+        return show_record
+
+    @classmethod
+    def add_supressor(cls, logger_name, log_level, regexp, n):
+        """
+        Adds a log suppression rule for a specific logger.
+
+        Args:
+            logger_name (str): The name of the logger to which the suppression rule applies.
+            log_level (str or int): The log level at which the suppression rule applies.
+            regexp (str): A regular expression to match the log message.
+            n (int): The frequency of messages to display after the first occurrence.
+        """
+        specs = cls._modulename_to_filtersspec_map.get(logger_name)
+        if not specs:
+            specs = []
+            cls._modulename_to_filtersspec_map[logger_name] = specs
+        specs.append(LogSupressor._FilterSpec(logger_name, log_level, regexp, n))
+
+        # Apply our filter function to this logger, if it has not been applied already
+        logger = logging.getLogger(logger_name)
+        for filter in logger.filters:
+            if filter is LogSupressor._filter_record:
+                return # We aready added this filter
+        logger.addFilter(LogSupressor._filter_record)
+
+
+# Log configruation starts here
+#-----------------------------------------
 _setup_root_logger()
+
+# this was done to allow logging syntax that matches print statments syntax (since we auto-converted them to log messages) e.g. (print("hello", end="")
 logger = PrintLikeLogger(_setup_genesis_logger())
+
+

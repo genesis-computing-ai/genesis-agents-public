c48052790832af32c1ef5d08e249ba002d24101f
genesis-gh-mglickman <matthew.glickman@genesiscomputing.ai>
2024-11-19 19:32:27 -0500

    explain how use_external works

--- llm_openai/openai_utils.py
+++ llm_openai/openai_utils.py
@@ -2,7 +2,8 @@ import os
 from openai import AzureOpenAI, OpenAI
 from llm_anthropic.anthropic_extended_openai import ExtendedOpenAI
 
-
+# use_external will be used whenever we connect to an external API (xai, gemini, anthropic) that supports the OpenAI API
+# ExtendedOpenAI is a wrapper that makes Anthropic and Bedrock compatible with the OpenAI API
 def get_openai_client(use_external=False):# -> OpenAI | AzureOpenAI:
     if use_external and os.getenv("OPENAI_EXTERNAL_API_KEY"):
         client = ExtendedOpenAI(api_key=os.getenv("OPENAI_EXTERNAL_API_KEY"),

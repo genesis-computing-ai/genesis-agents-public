84acad0a35e3d78c1009e9a26d811ee9b2f654e4
genesis-gh-mglickman <matthew.glickman@genesiscomputing.ai>
2024-11-28 16:45:08 -0500

    snow works for bot config

--- api/genesis_api_demo.py
+++ api/genesis_api_demo.py
@@ -6,16 +6,18 @@ import os
 import json
 import argparse
 from datetime import datetime
+from snowflake.connector import SnowflakeConnection
 
-class GenesisMetadataStore(BaseModel):
+class GenesisMetadataStore():#BaseModel):
     scope: str
     def __init__(self, scope):
-        super().__init__(scope=scope)
+        #super().__init__(scope=scope)
+        self.scope = scope
     def insert_or_update_metadata(self, metadata_type: str, name: str, metadata: Dict[str, Any]):
         pass
-    def get_metadata(self, metadata_type: str, name: str) -> Dict[str, Any]:
+    def get_metadata(self, metadata_type: str, name: str, name2: str = None) -> Dict[str, Any]:
         pass
-    def get_all_metadata(self, metadata_type: str):
+    def get_all_metadata(self, metadata_type: str, name: str = None):
         pass
 
 class LocalMetadataStore(GenesisMetadataStore):
@@ -33,20 +35,75 @@ class LocalMetadataStore(GenesisMetadataStore):
         with open(f"{metadata_type_dir}/{name}.json", "w") as f:
             import json
             json.dump(metadata_dict, f)
-    def get_metadata(self, metadata_type: str, name: str) -> BaseModel:
+    def get_metadata(self, metadata_type: str, name: str, name2: str = None) -> BaseModel:
         try:
-            with open(f"{self.metadata_dir}/{self.scope}/{metadata_type}/{name}.json", "r") as f:
+            if name2:
+                file_path = f"{self.metadata_dir}/{self.scope}/{metadata_type}/{name}/{name2}.json"
+            else:
+                file_path = f"{self.metadata_dir}/{self.scope}/{metadata_type}/{name}.json"
+            with open(file_path, "r") as f:
                 metadata_dict = json.load(f)
                 # Dynamically instantiate the original object type
                 metadata_class = getattr(sys.modules[__name__], metadata_dict['type'])
                 return metadata_class(**metadata_dict)
         except FileNotFoundError:
             return None
-    def get_all_metadata(self, metadata_type: str):
+    def get_all_metadata(self, metadata_type: str, name: str = None):
         metadata_type_dir = f"{self.metadata_dir}/{self.scope}/{metadata_type}"
         if not os.path.exists(metadata_type_dir):
             return []
         return [json.load(open(f"{metadata_type_dir}/{f.name}", "r")) for f in os.scandir(metadata_type_dir) if f.is_file()]
+
+class SnowflakeMetadataStore(GenesisMetadataStore):
+    metadata_type_mapping: Dict[str, Tuple[str, str]] = {
+        "GenesisBot": ("BOT_SERVICING", "BOT_ID"),
+        #"ToolDefinition": ("tools", "name"),
+        "GenesisProject": ("PROJECTS", "PROJECT_ID"),
+        "GenesisProcess": ("PROCESSES", "PROCESS_ID"),
+        "GenesisNote": ("NOTEBOOK", "NOTE_ID"),
+        "GenesisKnowledge": ("KNOWLEDGE", "KNOWLEDGE_THREAD_ID"),
+        "GenesisHarvestResults": ("HARVEST_RESULTS", "SOURCE_NAME"),
+    }
+    conn: SnowflakeConnection = None
+
+    def __init__(self, scope):
+        super().__init__(scope)
+        self.conn = SnowflakeConnection(
+            account=os.getenv("SNOWFLAKE_ACCOUNT_OVERRIDE"),
+            user=os.getenv("SNOWFLAKE_USER_OVERRIDE"),
+            password=os.getenv("SNOWFLAKE_PASSWORD_OVERRIDE"),
+            database=scope,
+            warehouse=os.getenv("SNOWFLAKE_WAREHOUSE_OVERRIDE"),
+            role=os.getenv("SNOWFLAKE_ROLE_OVERRIDE")
+        )
+    def insert_or_update_metadata(self, metadata_type: str, name: str, metadata: BaseModel):
+        pass
+    def get_metadata(self, metadata_type: str, name: str) -> BaseModel:
+        cursor = self.conn.cursor()
+        table_name, filter_column = self.metadata_type_mapping.get(metadata_type, (None, None))
+        if not table_name:
+            raise ValueError(f"Unknown metadata type: {metadata_type}")
+        query = f"SELECT * FROM app1.{table_name} WHERE {filter_column} = \\\'{name}\\\'"
+        cursor.execute("call core.run_arbitrary('%s')" % query)
+        metadata_dict = cursor.fetchone()
+        if metadata_dict:
+            metadata_class = getattr(sys.modules[__name__], metadata_type)
+            metadata_dict = json.loads(metadata_dict[0])[0]
+            metadata_dict = {k: v if v is not None else '' for k, v in metadata_dict.items()}
+            return metadata_class(**metadata_dict)
+        else:
+            return None
+    def get_all_metadata(self, metadata_type: str):
+        cursor = self.conn.cursor()
+        table_name, filter_column = self.metadata_type_mapping.get(metadata_type, (None, None))
+        if not table_name:
+            raise ValueError(f"Unknown metadata type: {metadata_type}")
+        query = f"SELECT {filter_column} FROM app1.{table_name}"
+        cursor.execute("call core.run_arbitrary('%s')" % query)
+        metadata_list = cursor.fetchall()
+        metadata_list = json.loads(metadata_list[0][0])
+        metadata_list = [metadata[filter_column] for metadata in metadata_list]
+        return metadata_list
 
 class BotGuardrails(BaseModel):
     def preprocess(self, message):
@@ -61,16 +118,44 @@ class BotKnowledgeGuardrails(BotGuardrails):
         return response
 
 class GenesisBot(BaseModel):
-    bot_id: str
-    bot_name: str
-    bot_description: str
-    tool_list: List[str]
-    bot_implementation: str
-    docs: List[str]
-    guardrails: BotKnowledgeGuardrails = BotKnowledgeGuardrails()
-    client: Any = None
-
+    BOT_ID: str
+    BOT_NAME: str
+    #BOT_DESCRIPTION: str
+    #TOOL_LIST: List[str]
+    BOT_IMPLEMENTATION: str
+    FILES: str # List[str]
     
+    API_APP_ID: str
+    AUTH_STATE: str
+    AUTH_URL: str
+    AVAILABLE_TOOLS: str
+    BOT_AVATAR_IMAGE: str
+    BOT_INSTRUCTIONS: str
+    BOT_INTRO_PROMPT: str
+    BOT_SLACK_USER_ID: str
+    CLIENT_ID: str
+    CLIENT_SECRET: str
+    DATABASE_CREDENTIALS: str
+    RUNNER_ID: str
+    SLACK_ACTIVE: str
+    SLACK_APP_LEVEL_KEY: str
+    SLACK_APP_TOKEN: str
+    SLACK_CHANNEL_ID: str
+    SLACK_SIGNING_SECRET: str
+    SLACK_USER_ALLOW: str
+    TEAMS_ACTIVE: str
+    TEAMS_APP_ID: str
+    TEAMS_APP_PASSWORD: str
+    TEAMS_APP_TENANT_ID: str
+    TEAMS_APP_TYPE: str
+    UDF_ACTIVE: str
+
+    guardrails: BotKnowledgeGuardrails = BotKnowledgeGuardrails()
+
+    def __str__(self):
+        return f"GenesisBot(BOT_ID={self.BOT_ID}, BOT_NAME={self.BOT_NAME}, BOT_IMPLEMENTATION={self.BOT_IMPLEMENTATION})"
+
+
 class ToolDefinition(BaseModel):
     name: str
     description: str
@@ -132,6 +217,25 @@ class GenesisLocalServer(GenesisServer):
     def get_message(self, bot_id, thread_id):
         return "Message from thread_12345"
 
+class GenesisSnowflakeServer(GenesisServer):
+    def __init__(self, scope):
+        super().__init__(scope)
+        self.add_adapter(GenesisLocalAdapter())
+        self.conn = SnowflakeConnection(
+            account=os.getenv("SNOWFLAKE_ACCOUNT_OVERRIDE"),
+            user=os.getenv("SNOWFLAKE_USER_OVERRIDE"),
+            password=os.getenv("SNOWFLAKE_PASSWORD_OVERRIDE"),
+            database=scope,
+            warehouse=os.getenv("SNOWFLAKE_WAREHOUSE_OVERRIDE"),
+            role=os.getenv("SNOWFLAKE_ROLE_OVERRIDE")
+        )
+        self.cursor = self.conn.cursor()
+    def add_message(self, bot_id, message, thread_id):
+        self.cursor.execute("call genesis_bots.app1.submit_udf('%s', '%s', {'bot_id': '%s'})" % (message, thread_id, bot_id))
+    def get_message(self, bot_id, thread_id):
+        self.cursor.execute("call genesis_bots.app1.lookup_udf('%s', '%s')" % (thread_id, bot_id))
+        return self.cursor.fetchone()[0]
+
 class GenesisProcess(BaseModel):
     process_id: str
     process_name: str
@@ -147,6 +251,19 @@ class GenesisNote(BaseModel):
     note_content: str
     note_params: str
 
+class GenesisKnowledge(BaseModel):
+    timestamp: datetime
+    timestamp_ntz: datetime
+    thread_id: str
+    knowledge_thread_id: str
+    primary_user: str
+    bot_id: str
+    last_timestamp: datetime
+    thread_summary: str
+    user_learning: str
+    tool_learning: str
+    data_learning: str
+
 class GenesisAPI:
     def __init__(self, server_type, scope):
         self.server_type = server_type
@@ -154,17 +271,20 @@ class GenesisAPI:
         if server_type == "local":
             self.metadata_store = LocalMetadataStore(scope)
             self.registered_server = GenesisLocalServer(scope)
+        elif server_type == "snowflake":
+            self.metadata_store = SnowflakeMetadataStore(scope)
+            self.registered_server = GenesisSnowflakeServer(scope)
         else:
             raise ValueError("Remote server not supported yet")
     def register_bot(self, bot: GenesisBot):
-        self.metadata_store.insert_or_update_metadata("bot", bot.bot_id, bot)
+        self.metadata_store.insert_or_update_metadata("GenesisBot", bot.bot_id, bot)
     def get_bot(self, bot_id):
-        bot = self.metadata_store.get_metadata("bot", bot_id)
+        bot = self.metadata_store.get_metadata("GenesisBot", bot_id)
         #if bot:
         #    bot.set_client(self)
         return bot
     def get_all_bots(self):
-        return self.metadata_store.get_all_metadata("bot")
+        return self.metadata_store.get_all_metadata("GenesisBot")
     
     def register_tool(self, tool: ToolDefinition):
         self.metadata_store.insert_or_update_metadata("tool", tool.name, tool)
@@ -180,33 +300,52 @@ class GenesisAPI:
             raise ValueError("No server registered")
     
     def register_project(self, project: GenesisProject):
-        self.metadata_store.insert_or_update_metadata("project", project.project_id, project)
+        self.metadata_store.insert_or_update_metadata("GenesisProject", project.project_id, project)
     def get_project(self, project_id) -> GenesisProject:
-        return self.metadata_store.get_metadata("project", project_id)
+        return self.metadata_store.get_metadata("GenesisProject", project_id)
     def get_all_projects(self):
-        return self.metadata_store.get_all_metadata("project")
+        return self.metadata_store.get_all_metadata("GenesisProject")
     
+    def get_project_assets(self, project_id, asset_id):
+        return self.metadata_store.get_metadata("project_assets", project_id, asset_id)
+    def get_all_project_assets(self, project_id):
+        return self.metadata_store.get_all_metadata("project_assets", project_id)
+
     def register_process(self, process: GenesisProcess):
-        self.metadata_store.insert_or_update_metadata("process", process.process_id, process)
+        self.metadata_store.insert_or_update_metadata("GenesisProcess", process.process_id, process)
     def get_process(self, process_id) -> GenesisProcess:
-        return self.metadata_store.get_metadata("process", process_id)
+        return self.metadata_store.get_metadata("GenesisProcess", process_id)
     def get_all_processes(self):
-        return self.metadata_store.get_all_metadata("process")
+        return self.metadata_store.get_all_metadata("GenesisProcess")
     
     def register_note(self, note: GenesisNote):
-        self.metadata_store.insert_or_update_metadata("note", note.note_id, note)
+        self.metadata_store.insert_or_update_metadata("GenesisNote", note.note_id, note)
     def get_note(self, note_id) -> GenesisNote:
-        return self.metadata_store.get_metadata("note", note_id)
+        return self.metadata_store.get_metadata("GenesisNote", note_id)
     def get_all_notes(self):
-        return self.metadata_store.get_all_metadata("note")
+        return self.metadata_store.get_all_metadata("GenesisNote")
 
+    def get_harvest_results(self, source_name):
+        return self.metadata_store.get_all_metadata("harvest_results", source_name)
+    def get_all_harvest_results(self):
+        return self.metadata_store.get_all_metadata("harvest_results")
+    
+    def register_knowledge(self, knowledge: GenesisKnowledge):
+        self.metadata_store.insert_or_update_metadata("GenesisKnowledge", knowledge.knowledge_thread_id, knowledge)
+    def get_knowledge(self, thread_id) -> GenesisKnowledge:
+        return self.metadata_store.get_metadata("GenesisKnowledge", thread_id)
+    def get_all_knowledge(self):
+        return self.metadata_store.get_all_metadata("GenesisKnowledge")
+    
     def add_message(self, bot_id, message=None, thread_id=None) -> str:
         return self.registered_server.add_message(bot_id, message, thread_id)
     def get_response(self, bot_id, thread_id=None) -> str:
         return self.registered_server.get_message(bot_id, thread_id)
 
 def main():
-    client = GenesisAPI("local", scope="GENESIS_INTERNAL") # or "url of genesis server" with a valid API key
+    #client = GenesisAPI("local", scope="GENESIS_INTERNAL") # or "url of genesis server" with a valid API key
+    client = GenesisAPI("snowflake", scope="GENESIS_BOTS_ALPHA") # or "url of genesis server" with a valid API key
+
     #new_bot = GenesisBot(bot_name="my_bot", bot_description="my bot description", tool_list=["send_slack_message", "run_sql_query"], 
     #                     bot_implementation="openai", docs=["doc1.txt", "doc2.txt"])
     #client.register_bot(new_bot)
@@ -283,6 +422,24 @@ def main():
     parser_get_response.add_argument('--bot_id', required=True, help='ID of the bot')
     parser_get_response.add_argument('--thread_id', required=False, help='Thread ID for the response')
 
+    # Harvest Results
+    parser_get_harvest_results = subparsers.add_parser('get_harvest_results', help='Get harvest results by source name')
+    parser_get_harvest_results.add_argument('--source_name', required=True, help='Source name for the harvest results')
+
+    # Get all harvest results
+    parser_get_all_harvest_results = subparsers.add_parser('get_all_harvest_results', help='Get all harvest results')
+
+    # Register knowledge
+    parser_register_knowledge = subparsers.add_parser('register_knowledge', help='Register new knowledge')
+    parser_register_knowledge.add_argument('--knowledge_thread_id', required=True, help='Thread ID of the knowledge')
+
+    # Get knowledge
+    parser_get_knowledge = subparsers.add_parser('get_knowledge', help='Get knowledge by thread ID')
+    parser_get_knowledge.add_argument('--thread_id', required=True, help='Thread ID of the knowledge')
+
+    # Get all knowledge
+    parser_get_all_knowledge = subparsers.add_parser('get_all_knowledge', help='Get all registered knowledge')
+
     while True:
         try:
             args = parser.parse_args(input("Enter a command: ").split())
@@ -338,6 +495,21 @@ def main():
         elif args.command == 'get_response':
             response = client.get_response(args.bot_id, args.thread_id)
             print(response)
+        elif args.command == 'get_harvest_results':
+            harvest_results = client.get_harvest_results(args.source_name)
+            print(harvest_results)
+        elif args.command == 'get_all_harvest_results':
+            all_harvest_results = client.get_all_harvest_results()
+            print(all_harvest_results)
+        elif args.command == 'register_knowledge':
+            new_knowledge = GenesisKnowledge(knowledge_thread_id=args.knowledge_thread_id)
+            client.register_knowledge(new_knowledge)
+        elif args.command == 'get_knowledge':
+            knowledge = client.get_knowledge(args.thread_id)
+            print(knowledge)
+        elif args.command == 'get_all_knowledge':
+            all_knowledge = client.get_all_knowledge()
+            print(all_knowledge)
         elif args.command is None:
             print("Invalid command. Type 'help' for available commands.")
         else:

f37d99da77b9a926700434353f2bd46094bb66e1
genesis-gh-jdavidson <jeff.davidson@genesiscomputing.ai>
2024-12-31 15:34:49 -0800

    checkpoint

--- core/bot_os_tools.py
+++ core/bot_os_tools.py
@@ -1300,46 +1300,6 @@ def get_tools(
     return func_descriptors, available_functions, tool_to_func_descriptors_map
 
 
-class BotOsDispatchInputAdapter(BotOsInputAdapter):
-    def __init__(self, bot_id) -> None:
-        bot_config = get_bot_details(bot_id=bot_id)
-        self.session = make_session_for_dispatch(bot_config)
-        self.tasks = {}
-
-    # allows for polling from source
-    def add_event(self, event):
-        pass
-
-    # allows for polling from source
-    def get_input(self, thread_map=None, active=None, processing=None, done_map=None):
-        pass
-
-    # allows response to be sent back with optional reply
-    def handle_response(
-        self,
-        session_id: str,
-        message: BotOsOutputMessage,
-        in_thread=None,
-        in_uuid=None,
-        task_meta=None,
-    ):
-        if message.status == "completed":
-            self.tasks[message.thread_id]["result"] = message.output
-
-    def dispatch_task(self, task):
-        # thread_id = self.session.add_task(task, self)
-        thread_id = self.session.create_thread(self)
-        self.tasks[thread_id] = {"task": task, "result": None}
-        self.session.add_message(BotOsInputMessage(thread_id=thread_id, msg=task))
-
-    def check_tasks(self):
-        self.session.execute()
-        if all(task["result"] is not None for task in self.tasks.values()):
-            return [task["result"] for task in self.tasks.values()]
-        else:
-            return False
-
-
 def dispatch_to_bots(task_template, args_array, dispatch_bot_id=None):
     """
     Dispatches a task to multiple bots, each instantiated by creating a new thread with a specific task.
@@ -1352,7 +1312,7 @@ def dispatch_to_bots(task_template, args_array, dispatch_bot_id=None):
     Returns:
         list: An array of responses.
     """
-
+    from bot_os_dispatch_input_adapter import BotOsDispatchInputAdapter
     if len(args_array) < 2:
         return "Error: args_array size must be at least 2."
 
@@ -1371,7 +1331,6 @@ def dispatch_to_bots(task_template, args_array, dispatch_bot_id=None):
             logger.info(f"dispatch_to_bots - {responses}")
             return responses
         time.sleep(1)
-
 
 BOT_DISPATCH_DESCRIPTIONS = [
     {
@@ -1417,76 +1376,3 @@ BOT_DISPATCH_DESCRIPTIONS = [
 ]
 
 bot_dispatch_tools = {"_delegate_work": "tool_belt.delegate_work"}
-
-
-def make_session_for_dispatch(bot_config):
-    input_adapters = []
-    bot_tools = json.loads(bot_config["available_tools"])
-
-    # Create a DB connector for this session.
-    # TODO - use the utility functions in the connectors module to DRY up.
-    genesis_source = os.getenv("GENESIS_SOURCE", default="BigQuery")
-
-    if genesis_source == "BigQuery":
-        credentials_path = os.getenv(
-            "GOOGLE_APPLICATION_CREDENTIALS", default=".secrets/gcp.json"
-        )
-        with open(credentials_path) as f:
-            connection_info = json.load(f)
-        # Initialize BigQuery client
-        db_adapter = BigQueryConnector(connection_info, "BigQuery")
-    else:  # Initialize BigQuery client
-        db_adapter = SnowflakeConnector(connection_name="Snowflake")
-        connection_info = {"Connection_Type": "Snowflake"}
-
-    logger.info("---> CONNECTED TO DATABASE: ", genesis_source)
-    tools, available_functions, function_to_tool_map = get_tools(
-        bot_tools, db_adapter, include_slack=False
-    )  # FixMe remove slack adapter if
-
-    instructions = (
-        bot_config["bot_instructions"] + "\n" + BASE_BOT_INSTRUCTIONS_ADDENDUM
-    )
-    logger.info(instructions, f'{bot_config["bot_name"]}, id: {bot_config["bot_id"]}')
-
-    # TESTING UDF ADAPTER W/EVE and ELSA
-    # add a map here to track botid to adapter mapping
-
-    bot_id = bot_config["bot_id"]
-    if os.getenv("BOT_DO_PLANNING_REFLECTION"):
-        pre_validation = BASE_BOT_PRE_VALIDATION_INSTRUCTIONS
-        post_validation = BASE_BOT_VALIDATION_INSTRUCTIONS
-    else:
-        pre_validation = ""
-        post_validation = None
-    if os.getenv("BOT_BE_PROACTIVE", "False").lower() == "true":
-        proactive_instructions = BASE_BOT_PROACTIVE_INSTRUCTIONS
-    else:
-        proactive_instructions = ""
-    session = BotOsSession(
-        bot_config["bot_id"],
-        instructions=instructions + proactive_instructions + pre_validation,
-        validation_instructions=post_validation,
-        input_adapters=input_adapters,
-        knowledgebase_implementation=BotOsKnowledgeAnnoy_Metadata(
-            f"./kb_{bot_config['bot_id']}"
-        ),
-        file_corpus=(
-            URLListFileCorpus(json.loads(bot_config["files"]))
-            if bot_config["files"]
-            else None
-        ),
-        log_db_connector=db_adapter,  # Ensure connection_info is defined or fetched appropriately
-        tools=tools,
-        available_functions=available_functions,
-        all_tools=tools,
-        all_functions=available_functions,
-        all_function_to_tool_map=function_to_tool_map,
-        bot_id=bot_config["bot_id"],
-    )
-    # if os.getenv("BOT_BE_PROACTIVE").lower() == "true" and slack_adapter_local:
-    #      session.add_task("Check in with Michael Gold to see if he has any tasks for you to work on.",
-    ##                       thread_id=session.create_thread(slack_adapter_local))
-    #                       input_adapter=slack_adapter_local))
-
-    return session  # , api_app_id, udf_adapter_local, slack_adapter_local

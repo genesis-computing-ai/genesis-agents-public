8343c638b9f951c1d21e9eb199671b4f0183b444
Reza <reza.vaghefi@genesiscomputing.ai>
2025-01-05 22:28:14 -0800

    added a helper function to convert datetime to string

--- core/bot_os_utils.py
+++ core/bot_os_utils.py
@@ -3,6 +3,11 @@ from collections.abc import Iterable
 def is_iterable(obj):
     return isinstance(obj, Iterable) and not isinstance(obj, str)
 
+def datetime_to_string(date_time_obj, format="%Y-%m-%d %H:%M:%S"):
+    if type(date_time_obj) == str:
+        return date_time_obj
+    return date_time_obj.strftime(format)
+
 
 def tupleize(*args):
     """

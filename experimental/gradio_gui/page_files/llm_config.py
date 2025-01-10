3b01e843fdc188228b6b01791e89c48bc9677e8a
Michael Rainey <michael.rainey@genesiscomputing.ai>
2025-01-10 08:51:23 -0800

    cleanup old files

--- experimental/gradio_gui/page_files/llm_config.py
+++
@@ -1,7 +0,0 @@
-import gradio as gr
-
-def create_llm_interface():
-    with gr.Group():
-        gr.Markdown("## LLM Configuration")
-        gr.Markdown("This feature is not yet implemented in the Gradio version.")
-    return gr.Group()

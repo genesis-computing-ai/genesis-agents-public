8fb61f15af238f6099c4955537e64d96f655f428
Michael Rainey <michael.rainey@genesiscomputing.ai>
2025-01-22 12:21:25 -0800

    update upgrade for community deployment

--- apps/streamlit_gui/make_community_compute_pool.py
+++ apps/streamlit_gui/make_community_compute_pool.py
@@ -1,6 +1,6 @@
 import re
 import os
-from ..genesis_bots.core.logging_config import logger
+from genesis_bots.core.logging_config import logger
 
 def replace_genesis_bots(input_file, output_file):
     with open(input_file, 'r') as file:
@@ -26,7 +26,7 @@ def replace_genesis_bots_eai(input_file, output_file):
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Define input and output file paths relative to the script directory
-files_to_change = ['snowflake_app/setup_script.sql','streamlit_gui/Genesis.py','streamlit_gui/page_files/config_pool.py','streamlit_gui/page_files/start_service.py','streamlit_gui/page_files/start_stop.py']
+files_to_change = ['../genesis_server/deployments/snowflake_app/setup_script.sql','streamlit_gui/Genesis.py','streamlit_gui/page_files/config_pool.py','streamlit_gui/page_files/start_service.py','streamlit_gui/page_files/start_stop.py']
 for file in files_to_change:
     input_file = os.path.join(script_dir, file)
     output_file = os.path.join(script_dir, file)
@@ -36,7 +36,7 @@ for file in files_to_change:
     logger.info(f"Replacement complete. New file '{output_file}' created with GENESIS_COMMUNITY_POOL.")
 
 # Define input and output file paths relative to the script directory
-files_to_change = ['connectors/snowflake_connector/snowflake_connector.py','streamlit_gui/page_files/start_service.py']
+files_to_change = ['../genesis_bots/connectors/snowflake_connector/snowflake_connector.py','../genesis_bots/core/tools/image_tools.py']
 for file in files_to_change:
     input_file = os.path.join(script_dir, file)
     output_file = os.path.join(script_dir, file)

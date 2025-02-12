genesis-computing-ai/genesis-agents-public

commit dc71b4e41d478838047a0f770bc2bc7bb5b905bc

fix/remove available tools tbl (#225)
Remove the AVAILABLE_TOOLS table and any dependency on it.
Instead, fetch available (persistent) tools from memory (combining old-style and new-style tool registration).

Also

- Remove genesis_bots/development as it is not used/supported
- Add missing tool (func group) description and add a test to validate a description is always provided for the persistent tool
- Some cleanup of unused tool descriptions

genesis_bots/core/bot_os_dispatch_input_adapter.py
@@ -79,10 +79,7 @@ def make_session_for_dispatch(bot_config):
         connection_info = {"Connection_Type": "Snowflake"}
 
     logger.info("---> CONNECTED TO DATABASE: ", genesis_source)
-    tools, available_functions, function_to_tool_map = get_tools(
-        bot_tools, db_adapter, include_slack=False
-    )  # FixMe remove slack adapter if
-
+    tools, available_functions, function_to_tool_map = get_tools(which_tools=bot_tools, include_slack=False)  # FixMe remove include_slack
     instructions = (
         bot_config["bot_instructions"] + "\n" + BASE_BOT_INSTRUCTIONS_ADDENDUM
     )

genesis_bots/core/bot_os_tools.py
@@ -5,63 +5,38 @@ import random
 import string
 import threading
 import time
-from   typing                   import Any, Callable, Dict, List
-from genesis_bots.core.bot_os_web_access import WebAccess
+from   typing                   import Callable, List
 
 
-from   genesis_bots.core                     import global_flags
-from   genesis_bots.core.bot_os_tools2       import (ToolFuncDescriptor,
+from   genesis_bots.core.bot_os_tools2 \
+                                import (ToolFuncDescriptor,
+                                        ToolFuncGroupLifetime,
                                         get_global_tools_registry,
                                         get_tool_func_descriptor)
 
 # from   genesis_bots.llm.llm_openai.bot_os_openai import StreamingEventHandler
 
-from   genesis_bots.google_sheets.g_sheets   import (add_g_file_comment,
-                                        add_reply_to_g_file_comment,
-                                        find_g_file_by_name,
-                                        get_g_file_comments,
-                                        get_g_file_version,
-                                        get_g_file_web_link,
-                                        get_g_folder_directory, read_g_sheet,
-                                        write_g_sheet_cell_v4)
 
-import collections
-import re
-from   typing                   import Optional
 
 from   genesis_bots.bot_genesis.make_baby_bot \
                                 import (MAKE_BABY_BOT_DESCRIPTIONS,
                                         make_baby_bot_tools)
+from   genesis_bots.connectors.database_tools \
+                                import (notebook_manager_functions,
+                                        notebook_manager_tools)
 from   jinja2                   import Template
-from genesis_bots.connectors.database_tools import (
-    notebook_manager_functions,
-    notebook_manager_tools,
-)
-from genesis_bots.core.bot_os_web_access import (
-    web_access_tools,  # This is the ToolFuncGroup
-    get_web_access_functions  # Add this to get the actual functions
-)
 
 from   genesis_bots.schema_explorer.harvester_tools \
                                 import (harvester_tools_functions,
                                         harvester_tools_list)
-from   genesis_bots.slack.slack_tools        import slack_tools, slack_tools_descriptions
+from   genesis_bots.slack.slack_tools \
+                                import slack_tools, slack_tools_descriptions
 
 
-from   genesis_bots.core.bot_os_defaults     import (BASE_BOT_INSTRUCTIONS_ADDENDUM,
-                                        BASE_BOT_PRE_VALIDATION_INSTRUCTIONS,
-                                        BASE_BOT_PROACTIVE_INSTRUCTIONS,
-                                        BASE_BOT_VALIDATION_INSTRUCTIONS)
 
 from   genesis_bots.core.bot_os_tool_descriptions \
-                                import (
-                                        # data_dev_tools,
-                                        # data_dev_tools_functions,
-                                        # git_file_manager_functions,
-                                        # git_file_manager_tools,
-                                        process_runner_functions,
-                                        process_runner_tools,
-)
+                                import (process_runner_functions,
+                                        process_runner_tools)
 
 from   genesis_bots.connectors.snowflake_connector.snowflake_connector \
                                 import SnowflakeConnector
@@ -72,10 +47,12 @@ from   genesis_bots.connectors.snowflake_connector.snowflake_connector \
 #                                         )
 
 
-from   genesis_bots.core.file_diff_handler   import GitFileManager
-from   genesis_bots.core.logging_config      import logger
+from   genesis_bots.core.logging_config \
+                                import logger
 
-from  genesis_bots.core.tools.tool_helpers import get_sys_email, get_process_info, chat_completion
+from   genesis_bots.core.tools.tool_helpers \
+                                import (chat_completion, get_process_info,
+                                        get_sys_email)
 
 
 genesis_source = os.getenv("GENESIS_SOURCE", default="Snowflake")
@@ -1154,22 +1131,95 @@ class ToolBelt:
                 cursor.close()
 
     # ====== NOTEBOOK END ==========================================================================================
+
+# A collection of 'old style' tools information. These are tools that are not registered in the global tools registry
+# (AKA new style tools).
+# TODO: migrate all old style tools to the new style tools registry. Once we do that, we can refactor get_tools() below
+#       to use the new style tools registry.
+_old_style_tool_metadata = {
+    "slack_tools": {
+        "func_descriptors": slack_tools_descriptions,
+        "funcname_to_locator": slack_tools
+    },
+    "harvester_tools": {
+        "func_descriptors": harvester_tools_functions,
+        "funcname_to_locator": harvester_tools_list
+    },
+    "make_baby_bot": {
+        "func_descriptors": MAKE_BABY_BOT_DESCRIPTIONS,
+        "funcname_to_locator": make_baby_bot_tools
+    },
+    "process_runner_tools": {
+        "func_descriptors": process_runner_functions,
+        "funcname_to_locator": process_runner_tools
+    },
+    "notebook_manager_tools": {
+        "func_descriptors": notebook_manager_functions,
+        "funcname_to_locator": notebook_manager_tools
+    }
+}
+
+
+def get_all_tool_to_func_map(include_ephemeral_tools: bool = False) -> dict[str, list[str]]:
+    """
+    Retrieve metadata for all tools, optionally including ephemeral tools.
+
+    Args:
+        include_ephemeral_tools (bool): Whether to include ephemeral tools in the metadata.
+
+    Returns:
+        dict: A dictionary mapping tool (group) names to a list of the function names in this group.
+    """
+    all_tools_metadata = {}
+
+    # Include old style tools (always non-ephemeral)
+    for tool_group, metadata in _old_style_tool_metadata.items():
+        func_names = [func_desc["function"]["name"] for func_desc in metadata["func_descriptors"]]
+        all_tools_metadata[tool_group] = func_names
+
+    # Include new style tools from the global tools registry
+    reg = get_global_tools_registry()
+    group_lifetime_incl_filter = None
+    if not include_ephemeral_tools:
+        group_lifetime_incl_filter = ToolFuncGroupLifetime.PERSISTENT
+    all_tools_metadata.update(reg.get_tool_to_func_map(group_lifetime_incl_filter))
+
+    return all_tools_metadata
+
+
+def get_persistent_tools_descriptions() -> dict[str, str]:
+    """
+    Retrieves a map of all avaialble tool func group) names to their descriptions for tools that have a 'persistent' lifetime, sorted by tool name.
+
+    Returns:
+        dict: A dictionary where each key is the name of a tool (group) and the value is its description.
+    """
+    from genesis_bots.core.bot_os_tool_descriptions import _tools_data # holds the legacy tools (groups) descriptors
+    tools_data = {name: description for name, description in _tools_data}
+
+    registry = get_global_tools_registry()
+    for group in registry.list_groups():
+        if group.lifetime == ToolFuncGroupLifetime.PERSISTENT:
+            tools_data[group.name] = group.description
+
+    return dict(sorted(tools_data.items()))
+
+
 def get_tools(
     which_tools: list[str],
-    db_adapter,
-    slack_adapter_local=None,
+    db_adapter = None, # UNUSED
+    slack_adapter_local=None, # UNUSED
     include_slack: bool = True,
-    tool_belt=None
+    tool_belt=None # UNUSED
     ) -> tuple[list, dict, dict]:
     """
     Retrieve a list of tools (function groups), available functions, and a mapping of functions to tools based on the specified tool names.
+    
+    This function combines information from 'old style' and 'new style' tools (those which are registered in the global tools registry).
 
     Args:
         which_tools (list): A list of tool (function group) names to retrieve.
-        db_adapter: The database adapter to use (some functions we methods of db_adapter).
-        slack_adapter_local: The Slack adapter to use for Slack operations (optional).
         include_slack (bool): Whether to include Slack tools (default is True).
-        tool_belt: An optional tool belt instance to use.
 
     Returns:
         tuple: A tuple containing three elements:
@@ -1189,7 +1239,7 @@ def get_tools(
     #         for tool in which_tools
     #     ]
 
-    which_tools = [tool for tool in which_tools]  # if tool != "autonomous_functions"
+    which_tools = list(which_tools)
 
     for tool in which_tools:
         try:
@@ -1197,7 +1247,7 @@ def get_tools(
         except:
             tool_name = tool
 
-        # Resolve 'old style' tool names
+        # Canonicalize 'old style' tool names
         # ----------------------------------
         if tool_name == "bot_dispatch_tools" or tool_name == "bot_dispatch":
             tool_name = "delegate_work"
@@ -1208,57 +1258,17 @@ def get_tools(
         if tool_name == "data_dev_tools":
             tool_name = "jira_connector_tools" # FIXME
 
-        if include_slack and tool_name == "slack_tools":
-            func_descriptors.extend(slack_tools_descriptions)
-            available_functions_loaded.update(slack_tools)
-            tool_to_func_descriptors_map[tool_name] = slack_tools_descriptions
-        elif tool_name == "harvester_tools":
-            func_descriptors.extend(harvester_tools_functions)
-            available_functions_loaded.update(harvester_tools_list)
-            tool_to_func_descriptors_map[tool_name] = harvester_tools_functions
-        elif tool_name == "make_baby_bot":
-            func_descriptors.extend(MAKE_BABY_BOT_DESCRIPTIONS)
-            available_functions_loaded.update(make_baby_bot_tools)
-            tool_to_func_descriptors_map[tool_name] = MAKE_BABY_BOT_DESCRIPTIONS
-        elif tool_name == "process_runner_tools":
-            func_descriptors.extend(process_runner_functions)
-            available_functions_loaded.update(process_runner_tools)
-            tool_to_func_descriptors_map[tool_name] = process_runner_functions
-        elif tool_name == "notebook_manager_tools":
-            func_descriptors.extend(notebook_manager_functions)
-            available_functions_loaded.update(notebook_manager_tools)
-            tool_to_func_descriptors_map[tool_name] = notebook_manager_functions
-        # elif tool_name == "web_access_tools":
-        #     func_descriptors.extend(get_web_access_functions())
-        #     available_functions_loaded.update({
-        #         func.__name__: func for func in get_web_access_functions()
-        #     })
-        #     tool_to_func_descriptors_map[tool_name] = get_web_access_functions()
-        # elif tool_name == "snowflake_tools":
-        #     func_descriptors.extend(snowflake_functions)
-        #     available_functions_loaded.update(snowflake_tools)
-        #     tool_to_func_descriptors_map[tool_name] = snowflake_functions
-        # elif tool_name == "git_file_manager_tools":  # Add this section
-        #     func_descriptors.extend(git_file_manager_functions)
-        #     available_functions_loaded.update(git_file_manager_tools)
-        #     tool_to_func_descriptors_map[tool_name] = git_file_manager_functions
-        # elif tool_name == "webpage_downloader":
-        #     func_descriptors.extend(webpage_downloader_functions)
-        #     available_functions_loaded.update(webpage_downloader_tools)
-        #     tool_to_func_descriptors_map[tool_name] = webpage_downloader_functions
-        # dagster tools have been converted to 'new type' tools (see bot_os_tools2.py)
-        # elif tool_name == "dagster_tools":
-        #     func_descriptors.extend(dagster_tool_functions)
-        #     available_functions_loaded.update(dagster_tools)
-        #     tool_to_func_descriptors_map[tool_name] = dagster_tool_functions
-        # elif tool_name == "bot_dispatch_tools" or tool_name == "bot_dispatch":
-        #     func_descriptors.extend(BOT_DISPATCH_DESCRIPTIONS)
-        #     available_functions_loaded.update(bot_dispatch_tools)
-        #     tool_to_func_descriptors_map[tool_name] = BOT_DISPATCH_DESCRIPTIONS
-        # elif tool_name == "data_dev_tools":
-        #     func_descriptors.extend(data_dev_tools_functions)
-        #     available_functions_loaded.update(data_dev_tools)
+        # Skip loading slack tools if include_slack is False (backward compatible behavior) TODO: remove special case
+        if tool_name == "slack_tools" and not include_slack:
+            continue
 
+        # Lookup tool in _old_style_tool_metadata
+        global _old_style_tool_metadata
+        if tool_name in _old_style_tool_metadata:
+            tool_metadata = _old_style_tool_metadata[tool_name]
+            func_descriptors.extend(tool_metadata["func_descriptors"])
+            available_functions_loaded.update(tool_metadata["funcname_to_locator"])
+            tool_to_func_descriptors_map[tool_name] = tool_metadata["func_descriptors"]
         else:
             # Resolve 'new style' tool functions
             # (from tool functions registry)
@@ -1353,48 +1363,3 @@ def dispatch_to_bots(task_template, args_array, dispatch_bot_id=None):
             logger.info(f"dispatch_to_bots - {responses}")
             return responses
         time.sleep(1)
-
-BOT_DISPATCH_DESCRIPTIONS = [
-    {
-        "type": "function",
-        "function": {
-            "name": "_delegate_work",
-            "description": "Delegates a task to another bot (or self) and waits for a JSON response. Use this when you need to delegate work to another bot.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "prompt": {
-                        "type": "string",
-                        "description": "The instruction or prompt to send to the target bot"
-                    },
-                    "target_bot": {
-                        "type": "string",
-                        "description": "Bot ID or name of target bot to delegate to. If you dont know the exact ID or name, call with target_bot_id 'UNKNOWN' to get a list of active bots."
-                    },
-                    "max_retries": {
-                        "type": "integer",
-                        "description": "Maximum number of retry attempts (1-10), defaults to 3",
-                        "minimum": 1,
-                        "maximum": 10,
-                        "default": 3
-                    },
-                    "timeout_seconds": {
-                        "type": "integer",
-                        "description": "Maximum seconds to wait for response, defaults to 300",
-                        "minimum": 1,
-                        "default": 300
-                    }
-                    ,
-                    "callback_id": {
-                        "type": "string",
-                        "description": "Optional callback_id to continue a previous delegation thread. Use this if you need to follow up on a previous delegation, by providing the callback_id that you received in the response to the previous delegation. If not used, a new thread will be started with the target_bot for this delegation.",
-                        "default": None
-                    }
-                },
-                "required": ["prompt"]  # Only prompt is required, others have defaults
-            }
-        }
-    }
-]
-
-bot_dispatch_tools = {"_delegate_work": "tool_belt.delegate_work"}

genesis_bots/demo/sessions_creator.py
@@ -23,8 +23,7 @@ from   genesis_bots.core.bot_os_memory \
                                 import BotOsKnowledgeAnnoy_Metadata
 
 from   genesis_bots.bot_genesis.make_baby_bot \
-                                import (get_all_bots_full_details,
-                                        get_available_persistent_tools)
+                                import get_all_bots_full_details
 from   genesis_bots.core.bot_os_tools \
                                 import ToolBelt, get_tools
 from   genesis_bots.core.bot_os_tools2 \
@@ -40,7 +39,8 @@ from   genesis_bots.core.bot_os_task_input_adapter \
 from   genesis_bots.core.bot_os_udf_proxy_input \
                                 import UDFBotOsInputAdapter
 
-
+from   genesis_bots.core.bot_os_tools \
+                                import get_persistent_tools_descriptions
 
 from   genesis_bots.core.logging_config \
                                 import logger
@@ -98,7 +98,7 @@ def get_legacy_sessions(bot_id: str, db_adapter) -> dict | list:
     return threads
 
 
-def _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter):
+def _resolve_session_tools_info(bot_config):
     '''helper function for make_session(...) to resolve tools & tool-functions info for a given bot '''
 
     #NOTE on nameing: 'tools' are named groups of tool-functions
@@ -109,8 +109,8 @@ def _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter):
     slack_enabled = bot_config.get("slack_active", "Y") == "Y"
 
     # get the names of all tools (function groups) persistent in the DB (which are not bot-specific)
-    all_p_tools_descriptions = get_available_persistent_tools()
-    all_p_tools_names = [tool["tool_name"] for tool in all_p_tools_descriptions]
+    all_p_tools_descriptions = get_persistent_tools_descriptions()
+    all_p_tools_names = list(all_p_tools_descriptions.keys())
 
     logger.info(f"Number of all available persistent tools (listed in the DB) {len(all_p_tools_names)}")
 
@@ -135,10 +135,8 @@ def _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter):
     # get functions metadata for the (persistent) tools configured for this bot
     (available_p_func_descriptors,            # list of func descriptors dicts
      available_p_callables_map,               # map from func name to its callable
-     tool_to_func_descs_map                   # map from tool (group) name to a list of func descriptors
-     ) = get_tools(
-        list(bot_p_tool_names), slack_adapter_local=slack_adapter_local, db_adapter=db_adapter, tool_belt=tool_belt
-    )
+     _                                        # map from tool (group) name to a list of func descriptors
+     ) = get_tools(which_tools=list(bot_p_tool_names))
     logger.info(f"Number of available persistent functions for bot {bot_id}: {len(available_p_callables_map)}")
 
     # get ephemeral functions that are assigned to this bot and convert them to the same info structure as the persistent functions
@@ -163,9 +161,8 @@ def _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter):
     (all_func_descriptions,                 # list of func descriptors dicts
      all_callables_map,                     # map from func name to its callable
      all_tool_to_func_descs_map             # map from tool (group) name to a list of func descriptors
-     ) = get_tools(
-        all_tool_names, slack_adapter_local=slack_adapter_local, db_adapter=db_adapter, tool_belt=tool_belt
-    )
+     ) = get_tools(which_tools=all_tool_names)
+
     logger.info(f"Number of all persistent functions: {len(all_callables_map)}")
 
     # Return a simple object with the resolved tools and functions
@@ -314,7 +311,7 @@ def make_session(
             logger.error(f'Failed to create Slack adapter with the provided configuration for bot {bot_config["bot_name"]}')
 
     # Use _resolve_session_tools_info to get tool information
-    tools_info = _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter)
+    tools_info = _resolve_session_tools_info(bot_config)
 
     simple_mode = os.getenv("SIMPLE_MODE", "false").lower() == "true"
 

commit 05a2c5fa9050c65e2ee9c8a4a826404773d001a1

Fix a bug where old-style tools were not resolved to their callables. (#232)
This bug was introduced in #225

genesis_bots/core/bot_os_dispatch_input_adapter.py
@@ -79,7 +79,9 @@ def make_session_for_dispatch(bot_config):
         connection_info = {"Connection_Type": "Snowflake"}
 
     logger.info("---> CONNECTED TO DATABASE: ", genesis_source)
-    tools, available_functions, function_to_tool_map = get_tools(which_tools=bot_tools, include_slack=False)  # FixMe remove include_slack
+    tools, available_functions, function_to_tool_map = get_tools(which_tools=bot_tools,
+                                                                 db_adapter=db_adapter,
+                                                                 include_slack=False)  # FixMe remove include_slack
     instructions = (
         bot_config["bot_instructions"] + "\n" + BASE_BOT_INSTRUCTIONS_ADDENDUM
     )

genesis_bots/core/bot_os_tools.py
@@ -1207,10 +1207,10 @@ def get_persistent_tools_descriptions() -> dict[str, str]:
 
 def get_tools(
     which_tools: list[str],
-    db_adapter = None, # UNUSED
-    slack_adapter_local=None, # UNUSED
+    db_adapter = None, # used for 'old style' tools
+    slack_adapter_local=None, # # used for 'old style' tools
     include_slack: bool = True,
-    tool_belt=None # UNUSED
+    tool_belt=None # # used for 'old style' tools
     ) -> tuple[list, dict, dict]:
     """
     Retrieve a list of tools (function groups), available functions, and a mapping of functions to tools based on the specified tool names.
@@ -1309,6 +1309,7 @@ def get_tools(
             assert isinstance(function_handle, str)
             module_path, func_name = function_handle.rsplit(".", 1)
             if module_path in locals():
+                # old style tools might be depending on the following object: tool_belt, db_adapter, slack_adapter_local
                 module = locals()[module_path]
                 try:
                     func = getattr(module, func_name)
@@ -1327,7 +1328,10 @@ def get_tools(
                 module = __import__(module_path, fromlist=[func_name])
                 func = getattr(module, func_name)
                 # logger.info("imported: ",func)
-            available_functions[name] = func
+            if func is not None:
+                available_functions[name] = func
+            else:
+                logger.warning(f"Tool Function '{name}' could not be resolved to a callable from '{function_handle}'. It may be listed as a tool function, but not available for use.")
 
     return func_descriptors, available_functions, tool_to_func_descriptors_map
 

genesis_bots/demo/sessions_creator.py
@@ -98,10 +98,10 @@ def get_legacy_sessions(bot_id: str, db_adapter) -> dict | list:
     return threads
 
 
-def _resolve_session_tools_info(bot_config):
+def _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter):
     '''helper function for make_session(...) to resolve tools & tool-functions info for a given bot '''
 
-    #NOTE on nameing: 'tools' are named groups of tool-functions
+    #NOTE on naming: 'tools' are named groups of tool-functions
 
 
     # fetch some bot attributes that are needed for tool resolution
@@ -128,15 +128,23 @@ def _resolve_session_tools_info(bot_config):
     if not slack_enabled:
         bot_p_tool_names.discard("slack_tools")
 
-    # NOTE: Legacy? ToolBelt seems to be a local variable that is used as a global variable by some tools
-    # TODO: clean this up
+
+    # 'old-style' tools require some legacy args to resolve to their callables
     tool_belt = ToolBelt()
+    legacy_args = dict(
+        tool_belt = tool_belt,
+        db_adapter = db_adapter,
+        slack_adapter_local = slack_adapter_local,
+        include_slack = (slack_adapter_local is not None)
+    )
+
 
     # get functions metadata for the (persistent) tools configured for this bot
     (available_p_func_descriptors,            # list of func descriptors dicts
      available_p_callables_map,               # map from func name to its callable
      _                                        # map from tool (group) name to a list of func descriptors
-     ) = get_tools(which_tools=list(bot_p_tool_names))
+     ) = get_tools(which_tools=list(bot_p_tool_names), **legacy_args)
+
     logger.info(f"Number of available persistent functions for bot {bot_id}: {len(available_p_callables_map)}")
 
     # get ephemeral functions that are assigned to this bot and convert them to the same info structure as the persistent functions
@@ -161,7 +169,7 @@ def _resolve_session_tools_info(bot_config):
     (all_func_descriptions,                 # list of func descriptors dicts
      all_callables_map,                     # map from func name to its callable
      all_tool_to_func_descs_map             # map from tool (group) name to a list of func descriptors
-     ) = get_tools(which_tools=all_tool_names)
+     ) = get_tools(which_tools=all_tool_names, **legacy_args)
 
     logger.info(f"Number of all persistent functions: {len(all_callables_map)}")
 
@@ -174,7 +182,6 @@ def _resolve_session_tools_info(bot_config):
         all_callables_map = all_callables_map,
         all_tool_to_func_descs_map = all_tool_to_func_descs_map,
         ephemeral_bot_callables = ephemeral_bot_callables,
-
         tool_belt=tool_belt
     )
 
@@ -311,7 +318,7 @@ def make_session(
             logger.error(f'Failed to create Slack adapter with the provided configuration for bot {bot_config["bot_name"]}')
 
     # Use _resolve_session_tools_info to get tool information
-    tools_info = _resolve_session_tools_info(bot_config)
+    tools_info = _resolve_session_tools_info(bot_config, slack_adapter_local, db_adapter)
 
     simple_mode = os.getenv("SIMPLE_MODE", "false").lower() == "true"
 

genesis_bots/slack/slack_bot_os_adapter.py
@@ -62,10 +62,10 @@ class SlackBotAdapter(BotOsInputAdapter):
         try:
             logger.debug("Calling parent class constructor")
             super().__init__()
-            
+
             logger.debug("Initializing Slack App")
             self.slack_app = App(token=token, signing_secret=signing_secret)
-            
+
             logger.debug("Setting basic attributes")
             self.channel_id = channel_id
             self.handler = SlackRequestHandler(self.slack_app)
@@ -73,7 +73,7 @@ class SlackBotAdapter(BotOsInputAdapter):
             self.bot_user_id = bot_user_id
             self.user_info_cache = {}
             self.bot_name = bot_name
-            
+
             logger.debug("Initializing tracking dictionaries")
             self.last_message_id_dict = {}
             self.thinking_map = {}
@@ -86,7 +86,7 @@ class SlackBotAdapter(BotOsInputAdapter):
             self.finalized_threads = {}
             self.split_at = 3700  # use 3700 normally
             self.legacy_sessions = legacy_sessions
-            
+
             logger.debug("Setting thread-related attributes to None")
             self.slack_thread = None # initialized later
             self.slack_thread_shutdown_event = None # # initialized later
@@ -95,9 +95,9 @@ class SlackBotAdapter(BotOsInputAdapter):
             logger.debug("Setting up log suppressors")
             for msg_re_to_suppress in [r"Failed to establish a connection", r"on_error invoked \(session id"]:
                 LogSupressor.add_supressor(
-                    self.slack_app.logger.name, 
+                    self.slack_app.logger.name,
                     log_level=logging.ERROR,
-                    regexp=msg_re_to_suppress, 
+                    regexp=msg_re_to_suppress,
                     n=100
                 )
 
@@ -209,7 +209,7 @@ class SlackBotAdapter(BotOsInputAdapter):
                     self.slack_thread = threading.Thread(target=run_slack_app)
                     self.slack_thread_shutdown_event = threading.Event() # used for signalling the thread to terminate
                     self.slack_thread.start()
-                    
+
                 except Exception as e:
                     logger.error(f"Failed to initialize socket mode: {str(e)}", exc_info=True)
                     raise
@@ -1225,7 +1225,7 @@ class SlackBotAdapter(BotOsInputAdapter):
                 logger.error(
                     f"SlackBotAdapter:handle_response - Error posting message: {e}"
                 )
-                
+
     def process_attachments(self, msg, attachments, files_in = None):
         files_to_attach = []
         for attachment in attachments:
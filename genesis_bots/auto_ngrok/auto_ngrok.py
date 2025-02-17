c0f648b98518aac13117eb841509d365fc95310c
genesis-gh-jdavidson <jeff.davidson@genesiscomputing.ai>
2025-02-17 14:17:09 -0800

    Log NGROK endpoint if used

--- genesis_bots/auto_ngrok/auto_ngrok.py
+++ genesis_bots/auto_ngrok/auto_ngrok.py
@@ -41,20 +41,20 @@ def start_ngrok():
         try:
             # Set auth token
             ngrok.set_auth_token(NGROK_AUTH_TOKEN)
-            
+
             # If we don't have both tunnels, create new ones
             initial_tunnel = ngrok.connect(
                 addr="http://localhost:8080",
                 proto="http",
                 name="web-tunnel"
             )
-            
+
             listener_3978 = ngrok.connect(
                 addr="http://localhost:3978",
                 proto="http",
                 name="bot-tunnel"
             )
-            
+
             return initial_tunnel.url(), listener_3978.url()
         except Exception as e:
             logger.info(f"Error establishing NGROK connection: {e}")
@@ -73,7 +73,7 @@ def launch_ngrok_and_update_bots(update_endpoints=False):
         if existing_listeners:
             logger.info('Ngrok already running, skipping start')
             return True
-            
+
         # If no listeners exist, start new ones
         ngrok_urls = start_ngrok()
 
@@ -81,6 +81,8 @@ def launch_ngrok_and_update_bots(update_endpoints=False):
             update_bot_endpoints(new_base_url=ngrok_urls[0], runner_id=os.getenv('RUNNER_ID', 'jl-local-runner'))
 
         if ngrok_urls is not False:
+            logger.info(f'NGROK Web endpoint (port 8080): {ngrok_urls[0]}')
+            logger.info(f'NGROKBot endpoint (port 3978): {ngrok_urls[1]}')
             os.environ['NGROK_BASE_URL_8080'] = ngrok_urls[0]
             os.environ['NGROK_BASE_URL'] = ngrok_urls[0]
             os.environ['NGROK_BASE_URL_3978'] = ngrok_urls[1]
